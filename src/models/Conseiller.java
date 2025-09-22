972ab9fd32f6bbaeb7ef749c151acf1315e8679c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-09-22 17:23:04 +0100

    Add Conseiller class to represent advisor entities extending Person

---
+++ src/models/Conseiller.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Conseiller extends Person {
+    public Conseiller(int id, String nom, String prenom, String email) {
+        super(id, nom, prenom, email);
+    }
+}
