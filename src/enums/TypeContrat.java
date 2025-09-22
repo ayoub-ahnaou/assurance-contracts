c247cf5f31f4ff79ab994e516bcd204ce19a34da
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-09-22 17:23:20 +0100

    Add TypeContrat and TypeSinistre enums to represent contract and claim types

---
+++ src/enums/TypeContrat.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum TypeContrat {
+    AUTOMOBILE,
+    IMMOBILIER,
+    MALADIE
+}
---
+++ src/enums/TypeSinistre.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum TypeSinistre {
+    ACCIDENT_DE_VOITURE,
+    ACCIDENT_DE_MAISON,
+    MALADIE
+}
