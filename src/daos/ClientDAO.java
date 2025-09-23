4db01487ce56cfc6e1f753e77e8fe2b8bb3bcf1b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-09-23 15:25:28 +0100

    Refactor ClientDAO to use String for client ID and implement createClient method

--- src/daos/ClientDAO.java
+++ src/daos/ClientDAO.java
@@ -1,21 +1,39 @@
 package daos;
 
+import config.Database;
 import models.Client;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
 
 public class ClientDAO {
-    private Map<Integer, Client> clients = new HashMap<>();
+    private Map<String, Client> clients = new HashMap<>();
 
     // insert client into database
-    public void createClient(Client client) {}
+    public void createClient(Client client) {
+        try {
+            Connection connection = Database.getConnection();
+            String sql = "INSERT INTO clients (id, nom, prenom, email, conseiller_id) VALUES (?, ?, ?, ?, ?)";
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, client.getId());
+            stm.setString(2, client.getNome());
+            stm.setString(3, client.getPrenom());
+            stm.setString(4, client.getEmail());
+            stm.setString(5, client.getConseiller_id());
+
+            stm.executeUpdate();
+            System.out.println("Client created...");
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
     // get one account details
-    public Client getAccount(int id) {
-        return clients.get(id);
+    public Optional<Client> getAccount(String id) {
+        return Optional.of(clients.get(id));
     }
 
     // get all accounts
