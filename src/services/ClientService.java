ce263f3d90ac42be0af4d631caa7b4b0e79e22aa
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-09-23 15:25:35 +0100

    Refactor createClient method to accept Client object and validate fields

--- src/services/ClientService.java
+++ src/services/ClientService.java
@@ -4,16 +4,27 @@ import daos.ClientDAO;
 import models.Client;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ClientService {
     private ClientDAO dao = new ClientDAO();
 
-    public void createClient(int id, String nom, String prenom, String email) {
-        Client client = new Client(id, nom, prenom, email);
+    public void createClient(Client client) {
+        if(client.getNome() == null || client.getNome().isEmpty()) {
+            throw new IllegalArgumentException("Nom required..");
+        }
+        if(client.getPrenom() == null || client.getPrenom().isEmpty()) {
+            throw new IllegalArgumentException("Prenom required..");
+        }
+        if(client.getEmail() == null || client.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("Email required..");
+        }
+        // check for conseillerId existance in db before inserting
+        // search for it by conseillerDAO
         dao.createClient(client);
     }
 
-    public Client getAccount(int id) {
+    public Optional<Client> getAccount(String id) {
         return dao.getAccount(id);
     }
 
