f3e2a3aa5a46d7a1ae044450988a53ebffc6c375
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-09-23 15:26:27 +0100

    Replace Database connection with MainView initialization and menu display in main method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import config.Database;
+import views.MainView;
 
 public class Main {
     public static void main(String[] args) {
-        Database.getConnection();
+        MainView view = new MainView();
+        view.MenuPrincipal();
     }
 }
